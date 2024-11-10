91b1caf17eee807adcff124aea4b4be98ad5ccca
Олег Едисеев <ediseev.oleg@wb.ru>
2024-11-10 05:10:55 +0300

    lab1 finalfinalfinal

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <string>
 #include <map>
-#include <format>
+#include <sstream>
 
 using namespace std;
 
@@ -16,167 +16,185 @@ class Person {
         string name;
 };
 
-class Course {
+class CourseBase {
     public:
-        explicit Course(const string& name) : name(name) {}
-        string getName() const {
-            return name;
-        }
-    private:
-        string name;
+        virtual ~CourseBase() = default;
+        virtual string getName() const = 0;
 };
 
-class Grade {
+class Course : public CourseBase {
     public:
-        Grade(const string& courseName, const int score) : courseName(courseName), score(score) {}
-        string getGradeInfo() const {
-            return "Курс: " + courseName + ", оценка: " + to_string(score);
-        }
+        explicit Course(const string& name) : name(name) {}
+        string getName() const override { return name; }
     private:
-        string courseName;
-        int score;
+        string name;
 };
 
 class Student : public Person {
     public:
         explicit Student(const string& name) : Person(name) {}
-        void enroll(Course& course) {
-            enrolledCourses.push_back(&course);
+        void enroll(const Course& course) {
+            enrolledCourses.push_back(course);
         }
-        vector<Course*> getCourses() const{
+        vector<Course> getCourses() const{
             return enrolledCourses;
         }
     private:
-        vector<Course*> enrolledCourses;
+        vector<Course> enrolledCourses;
 };
 
 class Teacher : public Person {
     public:
         explicit Teacher(const string& name) : Person(name) {}
-        static Course createCourse(const string& courseName) {
-            return Course(courseName);
+        void createCourse(const string& courseName) {
+            createdCourses.emplace_back(courseName);
         }
-        static Grade assignGrade(const Course& course, int score) {
-            return Grade(course.getName(), score);
+        vector<Course> getCourses() const{
+            return createdCourses;
         }
+    private:
+        vector<Course> createdCourses;
 };
 
-class Archive {
+class Grade {
     public:
-        static Archive& getInstance() {
-            static Archive instance;
-            return instance;
+        Grade(const Student& student, const Course& course, const int score)
+            : studentName(student.getName()), courseName(course.getName()), score(score) {}
+        string getStudentName() const { return studentName; }
+        string getCourseName() const { return courseName; }
+        int getScore() const { return score; }
+    private:
+        string studentName;
+        string courseName;
+        int score;
+};
+
+class ArchiveBase {
+    public:
+        virtual ~ArchiveBase() = default;
+        virtual void getArchiveInfo() const = 0;
+};
+
+class Archive : ArchiveBase {
+    public:
+        void saveRecord(const Teacher& teacher,  const Course& course, const Student& student, const int score) {
+            const Grade grade(student, course, score);
+            records[teacher.getName()][course.getName()].push_back(grade);
         }
-        void saveGrade(const string& studentName, const Grade& grade) {
-            studentGrades[studentName].push_back(grade);
-        }
-        void printGrades(const string& studentName) const {
-            if (const auto it = studentGrades.find(studentName); it != studentGrades.end()) {
-                cout << "Оценки студента " << studentName << ": " << endl;
-                for (const Grade& grade : it->second) {
-                    cout << grade.getGradeInfo() << endl;
+        void getArchiveInfo() const override {
+            for (const auto& teacherRecord : records) {
+                cout << "Преподаватель: " << teacherRecord.first << endl;
+                for (const auto& courseRecord : teacherRecord.second) {
+                    cout << "   Курс: " << courseRecord.first << endl;
+                    for (const auto& grade : courseRecord.second) {
+                        cout << "       студент: " << grade.getStudentName() << ", оценка: " << grade.getScore() << endl;
+                    }
                 }
-                cout << endl;
-            } else {
-                cout << "Для " << studentName << " оценки не найдены" << endl;
             }
         }
     private:
-        Archive() = default;
-        map<string, vector<Grade>> studentGrades;
+        map<string, map<string, vector<Grade>>> records;
 };
 
-void testSystem() {
-    Teacher teacher1("Гонтовой Сергей Викторович");
-    Teacher teacher2("Пардаев Акбар Анварович");
 
-    Course tkpo = teacher1.createCourse("Технология конструирования программного обеспечения");
-    Course urpo = teacher2.createCourse("Управление разработкой программного обеспечения");
-
-    Student student1("Олег Едисеев");
-    Student student2("Иван Кубанов");
-
-    student1.enroll(tkpo);
-    student1.enroll(urpo);
-    student2.enroll(tkpo);
-
-    Archive::getInstance().saveGrade(student1.getName(), teacher1.assignGrade(tkpo, 85));
-    Archive::getInstance().saveGrade(student1.getName(), teacher2.assignGrade(urpo, 90));
-    Archive::getInstance().saveGrade(student2.getName(), teacher2.assignGrade(urpo, 70));
-
-    Archive::getInstance().printGrades("Олег Едисеев");
-    Archive::getInstance().printGrades("Иван Кубанов");
-}
-
-void testConsoleApp() {
+void testConsole() {
     vector<Student> students;
     vector<Teacher> teachers;
     vector<Course> courses;
 
-    int numTeachers, numCourses, numStudents;
+    int numTeachers, numStudents;
 
     cout << "----------------Факультатив----------------" << endl;
 
-    cout << "Введите количество преподавателей: "; cin >> numTeachers;
-    for (int i = 0; i < numTeachers; ++i) {
-        string teacherName;
-        cout << "Введите имя преподавателя " << i + 1 << ": "; cin >> teacherName;
-        teachers.emplace_back(teacherName);
-    }
+    Archive archive;
 
-    cout << "Введите количество курсов: "; cin >> numCourses;
-    for (int i = 0; i < numCourses; ++i) {
-        string courseName;
-        cout << "Введите название курса "<< i + 1 << ": "; cin >> courseName;
-        courses.emplace_back(courseName);
-    }
+    cout << "Введите количество преподавателей: ";
+    cin >> numTeachers; cin.ignore();
+    if (numTeachers > 0) {
+        for (int i = 0; i < numTeachers; ++i) {
+            string teacherName;
+            cout << "Введите имя преподавателя " << i + 1 << ": ";
+            getline(cin, teacherName);
+            teachers.emplace_back(teacherName);
 
-    cout << "Введите количество студентов: "; cin >> numStudents;
-    for (int i = 0; i < numStudents; ++i) {
-        string studentName;
-        cout << "Введите имя студента "<< i + 1 << ": "; cin >> studentName;
-        students.emplace_back(studentName);
-    }
-    cout << endl;
+            int numCourses;
+            cout << "Введите количество курсов для преподавателя " << i + 1 << ": ";
+            cin >> numCourses; cin.ignore();
 
-    for (Student& student : students) {
-        cout << "Запись студента " << student.getName() << " на курсы" << endl;
-        int enroll_qty;
-
-        cout << "Введите количество курсов, на которое хотите записаться: "; cin >> enroll_qty;
-        for (int i = 0; i < enroll_qty; ++i) {
-            cout << "Выберите номер курса (от 0 до " << numCourses - 1 << "): ";
-            int course_i;
-            cin >> course_i;
-
-            if (course_i >= 0 && course_i < numCourses) {
-                student.enroll(courses[course_i]);
-                cout << "Студент " << student.getName() << " записался на курс " << courses[course_i].getName() << endl;
-            } else {
-                cout << "Неверный номер курса" << endl;
+            for (int j = 0; j < numCourses; ++j) {
+                string courseName;
+                cout << "Введите название курса №"<< j + 1 << ": ";
+                getline(cin, courseName);
+                teachers.back().createCourse(courseName);
             }
         }
-    }
 
-    for (Student& student : students) {
-        for (Course *course : student.getCourses()) {
-            int score;
-            cout << "Введите оценку для студента " << student.getName() << " по курсу " << course->getName() << " (0 - 100): ";
-            cin >> score;
-
-            Grade grade = teachers[0].assignGrade(*course, score);
-            Archive::getInstance().saveGrade(student.getName(), grade);
+        cout << "Введите количество студентов: ";
+        cin >> numStudents; cin.ignore();
+        for (int i = 0; i < numStudents; ++i) {
+            string studentName;
+            cout << "Введите имя студента №"<< i + 1 << ": ";
+            getline(cin, studentName);
+            students.emplace_back(studentName);
         }
-    }
-    cout << endl;
+        cout << endl;
 
-    cout << "----------------Оценки студентов----------------" << endl;
-    for (const auto& student : students) {
-        Archive::getInstance().printGrades(student.getName());
+        for (Student& student : students) {
+            cout << "Запись студента " << student.getName() << " на курсы" << endl;
+
+            for (int i = 0; i < teachers.size(); ++i) {
+                Teacher& teacher = teachers[i];
+                cout << "Преподаватель: " << teacher.getName() << endl;
+                const auto& courses = teacher.getCourses();
+
+                for (int j = 0; j < courses.size(); ++j) {
+                    cout << "  [" << j + 1 << "] " << courses[j].getName() << endl;
+                }
+                cout << "Введите номера курсов через пробел для записи студента " << student.getName() << " (0 для пропуска): ";
+                string line;
+                getline(cin, line);
+                stringstream ss(line);
+                int courseChoice;
+                while (ss >> courseChoice) {
+                    if (courseChoice > 0 && courseChoice <= courses.size()) {
+                        student.enroll(courses[courseChoice - 1]);
+                    }
+                }
+            }
+        }
+        cout << endl;
+
+        for (const Teacher& teacher : teachers) {
+            const auto& courses = teacher.getCourses();
+            for (const Course& course : courses) {
+                cout << "Ввод оценок для курса " << course.getName() << " преподавателя " << teacher.getName() << "." << endl;
+                for (const Student& student : students) {
+                    bool enrolled = false;
+                    for (const Course& enrolledCourse : student.getCourses()) {
+                        if (enrolledCourse.getName() == course.getName()) {
+                            enrolled = true;
+                            break;
+                        }
+                    }
+                    if (enrolled) {
+                        int grade;
+                        cout << "Введите оценку для студента " << student.getName() << " (0 - 100): ";
+                        cin >> grade;
+                        archive.saveRecord(teacher, course, student, grade);
+                    }
+                }
+            }
+        }
+        cout << endl;
+
+        cout << "----------------Оценки студентов----------------" << endl;
+        archive.getArchiveInfo();
+    } else {
+        cout << "Кол-во преподавателей не может быть < 0! Введите его заново ";
     }
 }
 
 int main() {
-    testConsoleApp();
+    testConsole();
     return 0;
 }
